bca6fe94ddef734d577efb06fbcee39b3efcc4d4
cec0948 <cec0958@gmail.com>
2021-03-12 11:41:48 +0900

    최 추가3번

---
+++ src/kosta/mvc/controller/ChoiController.java
@@ -0,0 +1,7 @@
+package kosta.mvc.controller;
+
+public class ChoiController {
+	public void test() {
+		System.out.println("최가추가");
+	}
+}
--- src/kosta/mvc/service/TestService.java
+++ src/kosta/mvc/service/TestService.java
@@ -1,5 +1,7 @@
 package kosta.mvc.service;
 
 public class TestService {
-
+	public void bb() {
+		System.out.println("짜라자라짜짜짜");
+	}
 }
