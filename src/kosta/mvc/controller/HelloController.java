396739d3d4492b2fe06b57252ab1e30215e7f276
cec0948 <cec0958@gmail.com>
2021-03-12 12:15:49 +0900

    충돌실험

--- src/kosta/mvc/controller/HelloController.java
+++ src/kosta/mvc/controller/HelloController.java
@@ -4,4 +4,10 @@ public class HelloController {
 	public void aa() {
 		System.out.println("헬로컨");
 	}
+	public void choi() {
+		System.out.println("충돌 코드 실험");
+	}
+	public void cc() {
+		System.out.println("충돌충돌충돌");
+	}
 }
